d6013b8873720ebebc358b6d4b11f86f49c65f39
adroullier <arndt@nive.co>
2013-03-04 13:23:38 +0100

    new dev version 0.9.9

--- nive/__init__.py
+++ nive/__init__.py
@@ -15,7 +15,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #----------------------------------------------------------------------
 
-__version__ = "0.9.8"
+__version__ = "0.9.9"
 __doc__ = """nive"""
 
 
